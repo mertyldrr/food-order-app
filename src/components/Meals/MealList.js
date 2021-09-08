6116840d6da223390cc2b709329805335e173c67
mertyldrr <mertyildirir14@gmail.com>
2021-09-08 11:54:05 +0300

    fetching data from firebase, loading and error states added

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Meal from './Meal';
 import Card from '../UI/Card';
 import { ListGroup } from 'react-bootstrap';
 
-export const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const MealList = () => {
 
-  const list = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const res = await fetch("https://food-order-84b3e-default-rtdb.europe-west1.firebasedatabase.app/meals.json");
+
+      if (!res.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const resData = await res.json();
+
+      const mealsArr = [];
+
+      for (const key in resData) {
+        mealsArr.push({
+          id: key,
+          name: resData[key].name,
+          description: resData[key].description,
+          price: resData[key].price
+        });
+      }
+      setMeals(mealsArr);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+
+
+
+  }, []);
+
+  if (isLoading) {
+    return (
+      <h2 style={{ fontFamily: 'Merienda One', textAlign: 'center' }}>Loading...</h2>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <h2 style={{ fontFamily: 'Merienda One', textAlign: 'center', color: 'red' }}>{httpError}</h2>
+    );
+  }
+
+  const list = meals.map((meal) => (
     <Meal key={meal.id} meal={meal} />
   ))
 
